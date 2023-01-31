ff9baab0dff193ff5bc1ae3212f84d3b6320ec55
swamiganga <123745163+swamiganga@users.noreply.github.com>
2023-01-31 19:47:17 +0530

    BasicTask

---
+++ DemoProject/src/myPackage/MyClass.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+public class MyClass {
+	
+	public static void main(String argu[]) {
+		
+		System.out.println("Test");
+		System.out.println("Suit");
+		System.out.println("Regrsstion");
+		System.out.println("Pom.xml");
+		System.out.println("UtilityClass");
+	}
+
+}
